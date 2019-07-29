22189180f21c2578efe6ccd2846b150b4161babc
zzzhim <3218566389@qq.com>
2019-07-29 23:40:47 +0800

    修改配置文件

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,29 +2,29 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-07-17 23:11:44
- * @LastEditTime: 2019-07-29 23:38:36
+ * @LastEditTime: 2019-07-29 23:40:30
  * @LastEditors: Please set LastEditors
  */
 export type Method =
-  | 'get'
-  | 'GET'
-  | 'delete'
-  | 'DELETE'
-  | 'head'
-  | 'HEAD'
-  | 'options'
-  | 'OPTIONS'
-  | 'post'
-  | 'POST'
-  | 'put'
-  | 'PUT'
-  | 'patch'
-  | 'PATCH'
+    | 'get'
+    | 'GET'
+    | 'delete'
+    | 'DELETE'
+    | 'head'
+    | 'HEAD'
+    | 'options'
+    | 'OPTIONS'
+    | 'post'
+    | 'POST'
+    | 'put'
+    | 'PUT'
+    | 'patch'
+    | 'PATCH'
 
 export interface AxiosRequestConfig {
-  url: string
-  method?: Method
-  // headers?: any
-  data?: any
-  params?: any
+    url: string
+    method?: Method
+    // headers?: any
+    data?: any
+    params?: any
 }
